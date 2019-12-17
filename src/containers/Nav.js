f688947216c26d958a1010ad2a064a4cf7ccca38
Sridevi <SiriPandu@Sridevis-MacBook-Pro.local>
2019-12-17 13:36:52 -0600

    us001 : Layout designing

---
+++ src/containers/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <div className = "nav">
+            <img className = "logo" src = "../logo192.png" alt = 'logo'/>
+        </div>
+    )
+}
+
+export default Nav;
